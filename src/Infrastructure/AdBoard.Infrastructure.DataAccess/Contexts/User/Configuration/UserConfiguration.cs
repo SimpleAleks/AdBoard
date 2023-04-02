ffcc2b6324cc2dfe8e093636a34e27607b7c2361
Ilya Alexandrov <pukisona12@mail.ru>
2023-04-02 16:40:43 +0300

    Added entities configurations and dbContext

---
+++ src/Infrastructure/AdBoard.Infrastructure.DataAccess/Contexts/User/Configuration/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdBoard.Infrastructure.DataAccess.Contexts.User.Configuration;
+
+/// <summary>
+/// Конфигурация EF Core сущности <see cref="Domain.User.User"/>
+/// </summary>
+public class UserConfiguration : IEntityTypeConfiguration<Domain.User.User>
+{
+    public void Configure(EntityTypeBuilder<Domain.User.User> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Name).HasMaxLength(64).IsRequired();
+
+        builder.HasMany(u => u.Adverts)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+    }
+}
