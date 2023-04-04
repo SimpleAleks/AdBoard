060494911c2254eea5e1d50e8d8257bc39176484
Ilya Alexandrov <pukisona12@mail.ru>
2023-04-04 09:10:05 +0300

    Created migrator service to auto update db from migrations

---
+++ src/Host/AdBoard.Host.Migrator/Program.cs
@@ -0,0 +1,30 @@
+using AdBoard.Host.Migrator;
+using AdBoard.Host.Migrator.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdBoard.Host.Migrator;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+
+        var host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddServices(context.Configuration);
+            })
+            .Build();
+
+        await MigrateDatabaseAsync(host.Services);
+        await host.RunAsync();
+    }
+
+    private static async Task MigrateDatabaseAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<MigratorDbContext>();
+        await context.Database.MigrateAsync();
+    }
+
+}
---
+++ src/Host/AdBoard.Host.Migrator/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using AdBoard.Host.Migrator.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdBoard.Host.Migrator;
+
+/// <summary>
+/// Методы расширения для добавления сервисов
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Добавление сервисов
+    /// </summary>
+    /// <param name="services">Сервисы</param>
+    /// <param name="configuration">Конфигурация</param>
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.ConfigureDbConnections(configuration);
+        return services;
+    }
+
+    /// <summary>
+    /// Конфигурация подключения к БД
+    /// </summary>
+    private static IServiceCollection ConfigureDbConnections(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgresAdBoardDb");
+        if (connectionString is null) throw new ArgumentNullException(nameof(connectionString));
+        services.AddDbContext<MigratorDbContext>(options => options.UseNpgsql(connectionString));
+        return services;
+    }
+}
